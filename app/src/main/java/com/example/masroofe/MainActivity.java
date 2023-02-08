bec411a0df2047e0cac5b375532e6511cb2ccd99
adam <adam@test.com>
2023-02-08 14:43:59 +0200

    test login

--- app/src/main/java/com/example/masroofe/MainActivity.java
+++ app/src/main/java/com/example/masroofe/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.masroofe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,12 +16,46 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgMonthsRecord, imgUserGuide, imgSetting;
     private FloatingActionButton ParAddButton;
 
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+    public static final String LOGIN = "LOGIN";
+    public static final String USERNAME = "USERNAME";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String FULLNAME = "FULLNAME";
+    public static final String EMAIL = "EMAIL";
+    public static final String BIRTHDATE = "BIRTHDATE";
+    public static final String FIXEDINCOME = "FIXEDINCOME";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         setupParActions();
+        setupReference();
+        checkUserLogin();
+    }
+
+    private void setupReference() {
+
+        //mawada ref
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = prefs.edit();
+    }
+
+    private void checkUserLogin() {
+        String login = prefs.getString(LOGIN, "false");
+        Log.e("shareprefs",prefs.getString(USERNAME, "test"));
+        Log.e("shareprefs",prefs.getString(PASSWORD, "test"));
+        Log.e("shareprefs",prefs.getString(FULLNAME, "test"));
+        Log.e("shareprefs",prefs.getString(BIRTHDATE, "test"));
+        Log.e("shareprefs",prefs.getString(EMAIL, "test"));
+        Log.e("shareprefs",prefs.getString(FIXEDINCOME, "test"));
+        if (login.equalsIgnoreCase("false")){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void setupParActions() {
