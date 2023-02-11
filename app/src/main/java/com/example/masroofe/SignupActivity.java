dee248d60132772f66f1f6fa876948e65942d233
adam <adam@test.com>
2023-02-12 00:48:01 +0200

    Finish sign up

--- app/src/main/java/com/example/masroofe/SignupActivity.java
+++ app/src/main/java/com/example/masroofe/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.masroofe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -31,6 +32,9 @@ public class SignupActivity extends AppCompatActivity {
     private SharedPreferences prefs;
     private SharedPreferences.Editor editor;
     public static final String LOGIN = "LOGIN";
+    public static final String USERNAME = "USERNAME";
+    public static final String FULLNAME = "FULLNAME";
+    public static final String EMAIL = "EMAIL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +45,7 @@ public class SignupActivity extends AppCompatActivity {
         //--References
         checkUserLogin();
         setupReference();
-        finishSetup();
-        nextSetup();
+        Setup();
     }
 
     private void checkUserLogin() {
@@ -51,16 +54,14 @@ public class SignupActivity extends AppCompatActivity {
         editor = prefs.edit();
 
         String login = prefs.getString(LOGIN, "false");
-        if (login.equalsIgnoreCase("true"))
-        {
+        if (login.equalsIgnoreCase("true")) {
             Intent intent = new Intent(SignupActivity.this, MainActivity.class);
             startActivity(intent);
         }
     }
 
     @Override
-    protected void onRestart()
-    {
+    protected void onRestart() {
         super.onRestart();
         checkUserLogin();
     }
@@ -75,87 +76,86 @@ public class SignupActivity extends AppCompatActivity {
         email = findViewById(R.id.emailSignup);
         password = findViewById(R.id.passwordSignup);
         repeatPassword = findViewById(R.id.passwordRepeatSignup);
+        textError = findViewById(R.id.error);
     }
 
-    private void finishSetup() {
+    private void Setup() {
         finishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                if (fullName.length() > 0 && username.length() > 0 && email.length() > 0 && password.length() > 0&& repeatPassword.length() > 0 )
-                {
-                    SignupUser(username.getText().toString(), password.getText().toString(), fullName.getText().toString(), email.getText().toString());
-                }
-                else
-                {
-                    Toast.makeText(SignupActivity.this, "أملئ جميع الحقول\n وحاول مرة أخرى!", Toast.LENGTH_SHORT).show();
-                    textError.setText("أملئ جميع الحقول وحاول مرة أخرى!");
-                }
+            public void onClick(View view) {
+                checkTextInformation("main");
             }
         });
-}
-
-
-    private void nextSetup() {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                checkTextInformation("fixedIncomeInfoActivity");
             }
         });
     }
 
-    private void SignupUser(String username, String password, String fullName, String email)
-    {
+    private void checkTextInformation(String nextActivity) {
+        textError.setText("");
+        if (fullName.length() > 0 && username.length() > 0 && email.length() > 0 && password.length() > 0 && repeatPassword.length() > 0) {
+            if (password.getText().toString().equals(repeatPassword.getText().toString())) {
+                textError.setText("");
+                SignupUser(username.getText().toString(), password.getText().toString(), fullName.getText().toString(), email.getText().toString(), nextActivity);
+            } else {
+                textError.setText("كلمات المرور غير متطابقة!");
+            }
+        } else {
+            textError.setText("أملئ جميع الحقول وحاول مرة أخرى!");
+        }
+    }
+
+    private void SignupUser(String username, String password, String fullName, String email, String nextActivity) {
         String url = "https://adam.s-matar.com/android-restAPI/signup.php";
         RequestQueue queue = Volley.newRequestQueue(SignupActivity.this);
         StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                try
-                {
+                try {
                     JSONObject jsonObject = new JSONObject(response);
-
-                    if(jsonObject.getString("error").equalsIgnoreCase("true"))
-                    {
+                    if (jsonObject.getString("error").equalsIgnoreCase("true")) {
                         textError.setText("اسم المستخدم أو البريد الإلكتروني موجود من قبل!");
-
-                        Toast.makeText(SignupActivity.this, "اسم المستخدم أو البريد الإلكتروني موجود من قبل!", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
+                        Toast.makeText(SignupActivity.this, "اسم المستخدم أو البريد الإلكتروني متكرر!", Toast.LENGTH_SHORT).show();
+                    } else {
                         editor.putString(LOGIN, "true");
+                        editor.putString(USERNAME, username);
+                        editor.putString(FULLNAME, fullName);
+                        editor.putString(EMAIL, email);
                         editor.commit();
                         Toast.makeText(SignupActivity.this, "تم إنشاء الحساب بنجاح", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                        startActivity(intent);
+                        if (nextActivity.equalsIgnoreCase("main")) {
+                            Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(SignupActivity.this, FixedIncomeInfoActivity.class);
+                            startActivity(intent);
+                        }
                     }
-                }
-                catch (JSONException e) {
-
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         }, new com.android.volley.Response.ErrorListener() {
             @Override
-            public void onErrorResponse(VolleyError error)
-            {
+            public void onErrorResponse(VolleyError error) {
                 Toast.makeText(SignupActivity.this, "" + error, Toast.LENGTH_SHORT).show();
             }
-        }){
+        }) {
             @Override
-            public String getBodyContentType()
-            {
+            public String getBodyContentType() {
                 return "application/x-www-form-urlencoded; charset=UTF-8";
             }
+
             @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String> params = new HashMap<String,String>();
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
 
                 params.put("username", username);
                 params.put("password", password);
-                params.put("fulName", fullName);
+                params.put("fullname", fullName);
                 params.put("email", email);
                 return params;
             }
