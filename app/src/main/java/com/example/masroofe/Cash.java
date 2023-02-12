ab84116333d5248976c532c6dc5ac68c921ab914
Ex_App <1190759@student.birzeit.edu>
2023-02-12 15:33:00 +0200

    mawada_commit

---
+++ app/src/main/java/com/example/masroofe/Cash.java
@@ -0,0 +1,27 @@
+package com.example.masroofe;
+
+public class Cash {
+    private double total_amount = 0;
+    private double monthly_amount = 0;
+
+    public Cash() {
+        this.total_amount = total_amount;
+        this.monthly_amount = monthly_amount;
+    }
+
+    public double getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(double total_amount) {
+        this.total_amount = total_amount;
+    }
+
+    public double getMonthly_amount() {
+        return monthly_amount;
+    }
+
+    public void setMonthly_amount(double monthly_amount) {
+        this.monthly_amount = monthly_amount;
+    }
+}
