80df9e43bff59992d3872544798de4ccbd0e50c1
adam <adam@test.com>
2023-02-13 12:16:14 +0200

    Finish Add account

--- app/src/main/java/com/example/masroofe/DefineActivity.java
+++ app/src/main/java/com/example/masroofe/DefineActivity.java
@@ -10,8 +10,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DefineActivity extends AppCompatActivity {
 
@@ -22,6 +34,7 @@ public class DefineActivity extends AppCompatActivity {
     private SharedPreferences prefs;
     private SharedPreferences.Editor editor;
     public static final String LOGIN = "LOGIN";
+    public static final String USERNAME = "USERNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +49,56 @@ public class DefineActivity extends AppCompatActivity {
     public void btn_defineActivity_OnClick(View view) {
         String type = spinner.getSelectedItem().toString();
         String name = accountName.getText().toString();
-        if(type == "مصروف"){
-            Expenses exp = new Expenses(name, 0, 0);
+        if (name.trim().length()>0){
+            if(type.equals("مصروف")){
+                AddAccount(name,"expenses",prefs.getString(USERNAME,""));
+            }
+            else if (type.equals("إيراد / دخل")){
+                AddAccount(name,"revenues",prefs.getString(USERNAME,""));
+            }
+        }else{
+            Toast.makeText(this, "أدخل اسم الحساب", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        }
-        else if (type == "إيراد / دخل"){
-            Revenues rev = new Revenues(name, 0 ,0);
-        }
+    private void AddAccount(String name, String table, String username) {
+        String url = "https://adam.s-matar.com/android-restAPI/addaccounts.php";
+        RequestQueue queue = Volley.newRequestQueue(DefineActivity.this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(jsonObject.getString("error").equalsIgnoreCase("true")){
+                        Toast.makeText(DefineActivity.this, "الحساب موجود من قبل!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(DefineActivity.this, "تم إضافة الحساب بنجاح", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(DefineActivity.this, "" + error, Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String,String>();
+
+                params.put("username", username);
+                params.put("name", name);
+                params.put("table", table);
+                return params;
+            }
+        };
+        queue.add(request);
     }
 
     private void setUp() {
